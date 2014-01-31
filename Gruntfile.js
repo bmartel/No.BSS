54dae6b30f527ef0c736184badb9a65a2c4d253a
bmartel <brandonmartel@gmail.com>
2014-01-31 02:03:41 -0600

    Added grunt auto minify/concatenation of files

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,17 +9,52 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['compass'],
-                options: {
-                    livereload: true,
+        concat: {
+            css: {
+                src: [
+                    'css/*.css'
+                ],
+                dest: 'css/combined/app.css'
+            },
+            js: {
+                src: [
+                    'js/*.js'
+                ],
+                dest: 'js/combined/app.js'
+            }
+        },
+        cssmin: {
+            css: {
+                src: 'css/combined/app.css',
+                dest: 'css/combined/app.min.css'
+            }
+        },
+        uglify: {
+            js: {
+                files: {
+                    'js/combined/app.js': ['js/combined/app.min.js']
                 }
-			}
+            }
+        },
+		watch: {
+			scss: {
+				files: '**/*.scss',
+				tasks: ['compass']
+			},
+            css:{
+                files: 'css/*.css',
+                tasks: ['concat:css', 'cssmin']
+            },
+            js:{
+                files: 'js/*.js',
+                tasks: ['concat:js', 'uglify']
+            }
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default',['watch']);
 }
